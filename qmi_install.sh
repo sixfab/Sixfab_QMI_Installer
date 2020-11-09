ac396a97d02924ea577e160b89412ac44a943366
saeedjohar <saeed_johar@yahoo.com>
2020-11-09 17:42:23 +0300

    1. typo fixed in if statements 2. usb driver installation added 3. qmi_wwan dowanload link added

--- qmi_install.sh
+++ qmi_install.sh
@@ -20,11 +20,11 @@ UDHCPC_DIR=/usr/share/udhcpc
 
 # service names
 service_reconnect=qmi_reconnect.service
-service_ModemManager = ModemManager
+service_ModemManager=ModemManager
 
 # clean old installation 
 status_reconnect="$(systemctl is-active $service_reconnect)"
-if ["$status"="active"]; then 
+if [ "$status" = "active" ]; then 
     systemctl stop $service_reconnect
     systemctl disable $service_reconnect
 fi
@@ -46,36 +46,38 @@ echo -e "${YELLOW}Create and Change directory to $INS_DIR ${SET}"
 mkdir -p $INS_DIR &&
 pushd $INS_DIR
 
-#TODO: Check USB driver and do installation if required
-:'
 echo -e "${YELLOW}Checking Kernel${SET}"
 case $(uname -r) in
     4.14*) echo $(uname -r) based kernel found
         echo "${YELLOW}Downloading source files${SET}"
-        wget https://github.com/sixfab/Sixfab_QMI_Installer/blob/master/src/4.14.zip -O drivers.zip #TODO: check link
+        wget https://github.com/sixfab/Sixfab_QMI_Installer/raw/master/src/v4.14.zip -O drivers.zip #TODO: check link
         unzip drivers.zip -d $INS_DIR && rm -r drivers.zip;;
     4.19*) echo $(uname -r) based kernel found 
         echo "${YELLOW}Downloading source files${SET}"
-        wget https://github.com/sixfab/Sixfab_QMI_Installer/blob/master/src/4.19.1.zip -O drivers.zip   #TODO: check link
+        wget https://github.com/sixfab/Sixfab_QMI_Installer/raw/master/src/v4.19.1.zip -O drivers.zip   #TODO: check link
         unzip drivers.zip -d $INS_DIR && rm -r drivers.zip;;
-    5.4*) echo $(uname -r) based kernel contains driver;;
+    5.4.5*) echo $(uname -r) based kernel contains driver;;
+    5.4.7*) echo $(uname -r) based kernel found 
+        echo "${YELLOW}Downloading source files${SET}"
+        wget https://github.com/sixfab/Sixfab_QMI_Installer/raw/master/src/v5.4.72.zip -O drivers.zip   #TODO: check link
+        unzip drivers.zip -d $INS_DIR && rm -r drivers.zip;;
     *) echo "${RED}Driver for $(uname -r) kernel not found${SET}";exit 1;
 esac
 
 echo -e "${YELLOW}Change directory to $INS_DIR/drivers${SET}"
-pushd $INS_DIR/drivers
-make && make install
-popd
-'
-        #TODO: Install qmi driver whatsoever
-# write the link here.
-wget -O qmi_wwan.zip 
+if [ -d "$INS_DIR/drivers" ];then
+    pushd $INS_DIR/drivers
+    make && make install
+    popd
+fi
+
+wget https://github.com/sixfab/Sixfab_QMI_Installer/raw/master/src/qmi_wwan.zip -O qmi_wwan.zip 
 unzip qmi_wwan.zip -d $INS_DIR && rm qmi_wwan.zip
 pushd $INS_DIR/qmi_wwan
 make && make install
 popd
 
-echo -e "${YELLOW}Downloading Connect Manager${SET}"
+echo -e "${YELLOW}Downloading Connection Manager${SET}"
 wget https://github.com/sixfab/Sixfab_QMI_Installer/raw/master/src/quectel-CM.zip -O quectel-CM.zip
 unzip quectel-CM.zip && rm quectel-CM.zip
 
@@ -93,7 +95,7 @@ popd
 
 # If ModemManager is isntalled and running, stop it as it will interfere 
 status_modemmanager="$(systemctl is-active $service_ModemManager)"
-if ["$status_modemmanager"="active"]; then 
+if [ "$status_modemmanager" = "active" ]; then 
     systemctl stop $service_ModemManager
     systemctl disable $service_ModemManager
 fi
